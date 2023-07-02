ee56c99cb5a87220da8bea79483c49bddad45ec5
edwardday7 <37816734+edwardday7@users.noreply.github.com>
2023-07-02 10:49:56 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
-from flask import render_template
+from flask import render_template, jsonify
 from app import app
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/health')
+def health():
+    return jsonify({'status': 'ok'}), 200
