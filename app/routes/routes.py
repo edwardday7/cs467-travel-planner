cd7f569144214aee5fcf5aaeb6df5e694a77931a
Edward Day <edwardday@Edwards-MacBook-Pro-2.local>
2023-07-10 16:30:13 -0400

    auth initial commit

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -11,6 +11,17 @@ def users():
     users = db.session.execute(db.select(User).order_by(User.name)).scalars()
     return render_template('users.html', users=users)
 
+@app.route("/login", methods=["GET"])
+def login():
+    # username = request.json.get("username", None)
+    # password = request.json.get("password", None)
+    # if username != "test" or password != "test":
+    #     return jsonify({"msg": "Bad username or password"}), 401
+
+    # access_token = create_access_token(identity=username)
+    # return jsonify(access_token=access_token)
+    return render_template('login.html')
+
 @app.route('/health')
 def health():
     return jsonify({'status': 'ok'}), 200
