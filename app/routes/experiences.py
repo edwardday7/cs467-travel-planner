ff0a02c08e114396f8d2fcee5ea5cfc0108e423f
Sterlinghv <stervioo@gmail.com>
2023-07-23 13:35:47 -0600

    fixes rating sorting

--- app/routes/experiences.py
+++ app/routes/experiences.py
@@ -3,29 +3,35 @@ from flask import make_response, redirect, render_template, request
 from flask_jwt_extended import get_jwt_identity, jwt_required
 from geoalchemy2 import WKTElement
 from app import app, db, container_client
-from app.models.models import Experience
+from app.models.models import Experience, Rating
 from sqlalchemy import desc, or_
 from geoalchemy2.shape import to_shape
+
+from sqlalchemy.sql import func
 
 @app.route('/experiences', methods=['GET'])
 def experiences():
     search = request.args.get('q')
     sort = request.args.get('sort')
-    
-    if search:
-        experiences = Experience.query.filter(or_(Experience.title.contains(search), Experience.description.contains(search)))
-    else:
-        experiences = Experience.query
 
-    #this is borked
+    if search:
+        experiences = db.session.query(Experience, func.avg(Rating.rating).label('average_rating')).\
+                      outerjoin(Rating, Experience.id == Rating.experience_id).\
+                      group_by(Experience.id).\
+                      filter(or_(Experience.title.contains(search), Experience.description.contains(search)))
+    else:
+        experiences = db.session.query(Experience, func.avg(Rating.rating).label('average_rating')).\
+                      outerjoin(Rating, Experience.id == Rating.experience_id).\
+                      group_by(Experience.id)
     if sort == 'highest_rating':
-        experiences = experiences.order_by(Experience.rating.desc())
+        experiences = experiences.order_by(db.desc('average_rating'))
     elif sort == 'lowest_rating':
-        experiences = experiences.order_by(Experience.rating.asc())
+        experiences = experiences.order_by('average_rating')
 
     experiences = experiences.all()
 
     return render_template('experiences.html', experiences=experiences)
+
 
 @app.route('/experience/<int:experience_id>', methods=['GET'])
 def experience_detail(experience_id):
